9f1b6c8bba0ac62c1814ea263e5ddadc56c6bbde
Jota Mario Miranda G <jotayes101@gmail.com>
2018-11-27 09:00:09 -0500

    login modal

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,12 +13,34 @@
       <v-btn class="pink--text font-weight-black" flat>Accesories</v-btn>
     </v-toolbar-items>
     <v-spacer></v-spacer>
-    <v-btn icon>
-      <v-badge color="pink" right>
-        <span slot="badge">!</span>
-        <v-icon class="pink--text">account_circle</v-icon>
-      </v-badge>
-    </v-btn>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-btn slot="activator" icon>
+        <v-badge color="pink" right>
+          <span slot="badge">!</span>
+          <v-icon class="pink--text">account_circle</v-icon>
+        </v-badge>
+      </v-btn>
+      <!-- <v-btn slot="activator" color="primary" dark>Open Dialog</v-btn> -->
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-form>
+              <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
+              <v-text-field prepend-icon="lock" name="password" label="Password" id="password" type="password"></v-text-field>
+            </v-form>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-btn icon>
       <v-badge color="pink" right>
         <span slot="badge">6</span>
@@ -32,6 +54,7 @@
   // import img1 from '../assets/logo1fefer2.png'
   export default {
     name: 'AppHeader',
+    dialog: false
   }
 </script>
 
